f52566b5e78959ec07a069659dd8b78f5e364ac5
yangyong <yangyong@yunjiglobal.com>
2019-02-18 17:41:37 +0800

    日志输入自测

--- src/test/java/com/redis/LoggerTest.java
+++ src/test/java/com/redis/LoggerTest.java
@@ -1,7 +1,7 @@
 package com.redis;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 日志输出测试类
@@ -10,7 +10,7 @@ public class LoggerTest {
     /**
      * 日志记录
      */
-    private static final Logger LOGGER = LogManager.getLogger(LoggerTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggerTest.class);
 
     public static void main(String[] args){
         LOGGER.info("test logger input");
